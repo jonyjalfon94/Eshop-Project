944c7c484c0b83834f3da248f38b2f5b43e74a11
Jonathan Jalfon <jonyjalfon94@gmail.com>
2020-10-09 01:41:19 +0300

    Add init script

---
+++ init.sh
@@ -0,0 +1,25 @@
+# Install Docker
+
+sudo yum update -y
+sudo yum install docker -y
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+# Install docker-compose
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
+# Install git
+
+sudo yum install git -yum
+sudo yum update -y
+
+# Clone repository
+
+git clone https://github.com/jonyjalfon94/eShopOnWeb.git
+cd eShopOnWeb
+docker-compose build
+docker-compose up
