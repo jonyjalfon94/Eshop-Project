2549ca4de47b971e6d2f467f4f3ab4df962cb54d
Jonathan Jalfon <jonyjalfon94@gmail.com>
2020-10-11 22:42:20 +0300

    Updated init script

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
   required_version = ">= 0.13"
   
   backend "s3" {
-    bucket = "eshop-backend-bucket2"
+    bucket = "eshop-backend-bucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
--- init.sh
+++ init.sh
@@ -7,21 +7,29 @@ sudo yum install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 
+# Pull container images
+
+docker pull jonyjalfon94/eshop-web
+docker pull jonyjalfon94/eshop-api
+docker run --name eshopweb --rm -it -p 5106:5106 jonyjalfon94/eshop-web
+docker run --name eshopapi --rm -it -p 5200:5200 jonyjalfon94/eshop-api
+
+
 # Install docker-compose
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-sudo chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
+# sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+# sudo chmod +x /usr/local/bin/docker-compose
+# ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# docker-compose --version
 
-# Install git
+# # Install git
 
-sudo yum install git -y
-sudo yum update -y
+# sudo yum install git -y
+# sudo yum update -y
 
-# Clone repository
+# # Clone repository
 
-git clone https://github.com/jonyjalfon94/eShopOnWeb.git
-cd eShopOnWeb
-sudo docker-compose build
-sudo docker-compose up -d
+# git clone https://github.com/jonyjalfon94/eShopOnWeb.git
+# cd eShopOnWeb
+# sudo docker-compose build
+# sudo docker-compose up -d
