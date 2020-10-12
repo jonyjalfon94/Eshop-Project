880a8d7b0605d5b32083826428e036b0338e9201
Jonathan Jalfon <jonyjalfon94@gmail.com>
2020-10-12 23:46:26 +0300

    Changed init script

--- init.sh
+++ init.sh
@@ -7,29 +7,39 @@ sudo yum install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 
-# Pull container images
+#Install docker-compose
 
-docker pull jonyjalfon94/eshop-web
-docker pull jonyjalfon94/eshop-api
-docker run --name eshopweb --rm -it -p 5106:5106 jonyjalfon94/eshop-web
-docker run --name eshopapi --rm -it -p 5200:5200 jonyjalfon94/eshop-api
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
 
+# Create docker-compose.yml file
+cat <<EOT >> docker-compose.yml
+version: '3.4'
+services:
+ eshopwebmvc:
+   image: jonyjalfon94/eshop-web
+   environment:
+     - ASPNETCORE_ENVIRONMENT=Docker
+     - ASPNETCORE_URLS=http://+:80
+   ports:
+     - "5106:80"
+   volumes:
+     - ~/.aspnet/https:/root/.aspnet/https:ro
+     - ~/.microsoft/usersecrets:/root/.microsoft/usersecrets:ro
+ eshoppublicapi:
+   image: jonyjalfon94/eshop-api
+   environment:
+     - ASPNETCORE_ENVIRONMENT=Docker
+     - ASPNETCORE_URLS=http://+:80
+   ports:
+     - "5200:80"
+   volumes:
+     - ~/.aspnet/https:/root/.aspnet/https:ro
+     - ~/.microsoft/usersecrets:/root/.microsoft/usersecrets:ro
+EOT
 
-# Install docker-compose
+# Run containers
 
-# sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-# sudo chmod +x /usr/local/bin/docker-compose
-# ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-# docker-compose --version
-
-# # Install git
-
-# sudo yum install git -y
-# sudo yum update -y
-
-# # Clone repository
-
-# git clone https://github.com/jonyjalfon94/eShopOnWeb.git
-# cd eShopOnWeb
-# sudo docker-compose build
-# sudo docker-compose up -d
+sudo docker-compose up -d
