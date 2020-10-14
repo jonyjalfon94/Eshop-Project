17a914091701cf661c05bedf720dc44fbd01611b
jonyjalfon94 <56407777+jonyjalfon94@users.noreply.github.com>
2020-10-14 17:55:24 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,45 +1,26 @@
 #!/bin/bash
 
-# Install Docker
+echo "-------Install Docker-------"
 
 sudo yum update -y
 sudo yum install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 
-#Install docker-compose
+echo "-------Install docker-compose-------"
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 
-# Create docker-compose.yml file
-cat <<EOT >> docker-compose.yml
-version: '3.4'
-services:
- eshopwebmvc:
-   image: jonyjalfon94/eshop-web
-   environment:
-     - ASPNETCORE_ENVIRONMENT=Docker
-     - ASPNETCORE_URLS=http://+:80
-   ports:
-     - "5106:80"
-   volumes:
-     - ~/.aspnet/https:/root/.aspnet/https:ro
-     - ~/.microsoft/usersecrets:/root/.microsoft/usersecrets:ro
- eshoppublicapi:
-   image: jonyjalfon94/eshop-api
-   environment:
-     - ASPNETCORE_ENVIRONMENT=Docker
-     - ASPNETCORE_URLS=http://+:80
-   ports:
-     - "5200:80"
-   volumes:
-     - ~/.aspnet/https:/root/.aspnet/https:ro
-     - ~/.microsoft/usersecrets:/root/.microsoft/usersecrets:ro
-EOT
+echo"-------Get docker-compose.yml and docker-compose.override.yml-------"
 
-# Run containers
+mkdir eshopDeploy
+cd eshopDeploy
+wget https://raw.githubusercontent.com/jonyjalfon94/eShopOnWeb/master/docker-compose.yml
+wget https://raw.githubusercontent.com/jonyjalfon94/eShopOnWeb/master/docker-compose.override.yml
+
+echo"-------Run containers-------"
 
 sudo docker-compose up -d
